8f5433c3e21b66ce8d8a0a60b6e9b0fc090ff8de
Brendan McCauley <brendanmccauley@MacBook-Pro.dlinkrouter>
2020-09-25 13:21:44 +0400

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyBv-UafTYyaEJqJrD8pyRJbSBO1kFTZhhg',
+	authDomain: 'clone-cce13.firebaseapp.com',
+	databaseURL: 'https://clone-cce13.firebaseio.com',
+	projectId: 'clone-cce13',
+	storageBucket: 'clone-cce13.appspot.com',
+	messagingSenderId: '94018823543',
+	appId: '1:94018823543:web:7cf6c26be1e2bd6ff9ad72',
+	measurementId: 'G-8WXXVX85ZG',
+};
